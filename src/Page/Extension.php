a82745cd250fe192fb68bea42691ff5a40abb3a2
Vít Kutný <vitkutny@outlook.com>
2014-05-27 02:50:04 +0200

    dev

--- src/Page/Extension.php
+++ src/Page/Extension.php
@@ -5,5 +5,17 @@ namespace WebEdit\Page;
 use WebEdit;
 
 final class Extension extends WebEdit\Extension {
-    
+
+    public function loadConfiguration() {
+        $builder = $this->getContainerBuilder();
+        $builder->addDefinition($this->prefix('repository'))
+                ->setClass('WebEdit\Page\Repository');
+        $builder->addDefinition($this->prefix('facade'))
+                ->setClass('WebEdit\Page\Facade');
+        $builder->addDefinition($this->prefix('control'))
+                ->setImplement('WebEdit\Page\Control\Factory');
+        $builder->addDefinition($this->prefix('form.control'))
+                ->setImplement('WebEdit\Page\Form\Control\Factory');
+    }
+
 }
