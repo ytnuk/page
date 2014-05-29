e9e7b70954c90fc79cc6a405b4d8265b8d036388
Vít Kutný <vitkutny@outlook.com>
2014-05-29 15:19:27 +0200

    dev

--- src/Page/Control.php
+++ src/Page/Control.php
@@ -2,10 +2,10 @@
 
 namespace WebEdit\Page;
 
-use WebEdit;
+use WebEdit\Entity;
 use WebEdit\Page\Form;
 
-final class Control extends WebEdit\Control {
+final class Control extends Entity\Control {
 
     public function __construct(Form\Control\Factory $form) {
         $this->form = $form;
