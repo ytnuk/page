a7c52c712cb7e79457d64aa1cc08488755885cbf
Vít Kutný <vitkutny@outlook.com>
2014-09-24 09:18:14 +0200

    dev

--- src/Page/Form/Control.php
+++ src/Page/Form/Control.php
@@ -2,44 +2,25 @@
 
 namespace WebEdit\Page\Form;
 
+use WebEdit\Database;
 use WebEdit\Form;
-use WebEdit\Menu;
 use WebEdit\Page;
 
 final class Control extends Form\Control
 {
 
     private $page;
-    private $facade;
     private $form;
-    private $menuRepository;
 
-    public function __construct($page, Page\Facade $facade, Form\Factory $form, Menu\Repository $menuRepository)
+    public function __construct($page, Database\Form\Factory $form)
     {
         $this->page = $page;
-        $this->facade = $facade;
         $this->form = $form;
-        $this->menuRepository = $menuRepository;
     }
 
     protected function createComponentForm()
     {
-        $form = $this->form->create($this->page);
-        $form['menu'] = new Menu\Form\Container;
-        $form['page'] = new Page\Form\Container;
-        $front = $this->menuRepository->getByUid('front');
-        $form['menu']['parent']->setItems($this->menuRepository->findChildren($front)->fetchPairs('id', 'title'));
-        if ($this->page) {
-            $form['page']->setDefaults($this->page->toArray());
-            $form['menu']->setDefaults([
-                'title' => $this->page->menu->title,
-                'parent' => $this->page->menu->parent->id
-            ]);
-        }
-        $form->onAdd[] = [$this->facade, 'add'];
-        $form->onEdit[] = [$this->facade, 'edit'];
-        $form->onDelete[] = [$this->facade, 'delete'];
-        return $form;
+        return $this->form->create($this->page ?: new Page\Entity);
     }
 
 }
