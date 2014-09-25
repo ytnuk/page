22ec5843f1672d248aead49d63031886f40dbaff
Vít Kutný <vitkutny@outlook.com>
2014-09-25 09:28:04 +0200

    dev

---
+++ src/Page/Form/Container.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace WebEdit\Page\Form;
+
+use WebEdit\Database;
+
+final class Container extends Database\Form\Container
+{
+
+}
--- src/Page/Form/Control.php
+++ src/Page/Form/Control.php
@@ -11,16 +11,20 @@ final class Control extends Form\Control
 
     private $page;
     private $form;
+    private $repository;
 
-    public function __construct($page, Database\Form\Factory $form)
+    public function __construct($page, Page\Repository $repository, Form\Factory $form)
     {
         $this->page = $page;
+        $this->repository = $repository;
         $this->form = $form;
     }
 
     protected function createComponentForm()
     {
-        return $this->form->create($this->page ?: new Page\Entity);
+        $form = $this->form->create($this->page);
+        $form['page'] = new Page\Form\Container($this->page ?: new Page\Entity, $this->repository);
+        return $form;
     }
 
 }
