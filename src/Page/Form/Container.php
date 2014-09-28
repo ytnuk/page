80685c43ad6ac2d4a384ce0718cce51bdd5817cc
Vít Kutný <vitkutny@outlook.com>
2014-09-28 20:50:36 +0200

    dev

--- src/Page/Form/Container.php
+++ src/Page/Form/Container.php
@@ -10,7 +10,17 @@ final class Container extends Database\Form\Container
     protected function attached($form)
     {
         parent::attached($form);
-        $this['menu']['link']->setDefaultValue(':Page:Presenter:view');
+        unset($this['menu']['link']);
+        unset($this['menu']['linkId']);
+    }
+
+    public function setValues($values, $a = FALSE)
+    {
+        $entity = parent::setValues($values);
+        //$entity->menu->parent = 1;
+        $entity->menu->link = ':Page:Presenter:view';
+        $entity->menu->linkId = $this->repository->persist($entity)->getId();
+        return $entity;
     }
 
 }
--- src/Page/Form/Control.php
+++ src/Page/Form/Control.php
@@ -11,26 +11,16 @@ final class Control extends Form\Control
 
     private $page;
     private $form;
-    private $repository;
 
-    public function __construct($page, Page\Repository $repository, Form\Factory $form)
+    public function __construct($page, Database\Form\Factory $form)
     {
         $this->page = $page;
-        $this->repository = $repository;
         $this->form = $form;
     }
 
     protected function createComponentForm()
     {
-        $form = $this->form->create();
-        $form['page'] = new Page\Form\Container($this->page ?: new Page\Entity, $this->repository);
-        if ($this->page) {
-            $form->addSubmit('edit', 'form.button.save');
-            $form->addSubmit('delete', 'form.button.delete')->setValidationScope(FALSE);
-        } else {
-            $form->addSubmit('add', 'form.button.add');
-        }
-        return $form;
+        return $this->form->create($this->page ?: new Page\Entity);
     }
 
 }
