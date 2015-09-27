98a892c26f1f1fd1fb31ce5e0c51f26463c83f95
Vít Kutný <vitkutny@icloud.com>
2015-09-27 17:42:17 +0200

    nextras/orm entity metadata: introduced new parser
    https://github.com/nextras/orm/commit/dfcbf5291eaa64559a8f8c8836f308573b99e549

--- src/Page/Entity.php
+++ src/Page/Entity.php
@@ -5,8 +5,8 @@ use Nextras;
 use Ytnuk;
 
 /**
- * @property Nextras\Orm\Relationships\OneHasOneDirected|Ytnuk\Translation\Entity $content {1:1d Ytnuk\Translation\Entity::$page primary}
- * @property Nextras\Orm\Relationships\OneHasOneDirected|Ytnuk\Menu\Entity $menu {1:1d Ytnuk\Menu\Entity::$page primary}
+ * @property Nextras\Orm\Relationships\OneHasOneDirected|Ytnuk\Translation\Entity $content {1:1d Ytnuk\Translation\Entity::$page, primary=true}
+ * @property Nextras\Orm\Relationships\OneHasOneDirected|Ytnuk\Menu\Entity $menu {1:1d Ytnuk\Menu\Entity::$page, primary=true}
  */
 final class Entity
 	extends Ytnuk\Orm\Entity
