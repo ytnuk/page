b60e7e6864ae41eb9570a026fe93c085329b98d7
Vít Kutný <vitkutny@icloud.com>
2015-10-25 23:55:10 +0100

    orm update

--- src/Page/Entity.php
+++ src/Page/Entity.php
@@ -5,6 +5,7 @@ use Nextras;
 use Ytnuk;
 
 /**
+ * @property int $id {primary}
  * @property Nextras\Orm\Relationships\OneHasOneDirected|Ytnuk\Translation\Entity $content {1:1d Ytnuk\Translation\Entity::$page, primary=true}
  * @property Nextras\Orm\Relationships\OneHasOneDirected|Ytnuk\Menu\Entity $menu {1:1d Ytnuk\Menu\Entity::$page, primary=true}
  */
