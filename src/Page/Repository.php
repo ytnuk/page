375927db4b56b82b088341746459f7ffa1ff2b11
Vít Kutný <vitkutny@icloud.com>
2015-12-05 01:03:18 +0100

    implement Route\Filter\Out for module entities

--- src/Page/Repository.php
+++ src/Page/Repository.php
@@ -1,11 +1,13 @@
 <?php
 namespace Ytnuk\Page;
 
+use Nette;
 use Nextras;
 use Ytnuk;
 
 final class Repository
 	extends Ytnuk\Orm\Repository
+	implements Ytnuk\Web\Domain\Router\Filter\Out
 {
 
 	public static function getEntityClassNames() : array
@@ -13,5 +15,27 @@ final class Repository
 		return [
 			Entity::class,
 		];
+	}
+
+	public function filterOut(
+		array $params,
+		array & $dependencies = []
+	) : array
+	{
+		if ($params['presenter'] === 'Page:Presenter' && $id = $params['id'] ?? NULL) {
+			if ($entity = $this->getById($id)) {
+				if ($entity instanceof Ytnuk\Cache\Provider) {
+					$dependencies = array_merge_recursive(
+						$dependencies,
+						[
+							Nette\Caching\Cache::TAGS => $entity->getCacheTags(),
+						]
+					);
+				}
+				$params['slug'] = Nette\Utils\Strings::webalize($entity);
+			}
+		}
+
+		return $params;
 	}
 }
