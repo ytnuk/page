df9272fe1666ea39fb29b41de4cb7c0a2db5357d
Vít Kutný <vitkutny@icloud.com>
2015-12-30 23:20:20 +0100

    nextras/orm: one sided relations, primary => isMain

--- src/Page/Entity.php
+++ src/Page/Entity.php
@@ -6,8 +6,8 @@ use Ytnuk;
 
 /**
  * @property int $id {primary}
- * @property Nextras\Orm\Relationships\OneHasOne|Ytnuk\Translation\Entity $content {1:1 Ytnuk\Translation\Entity::$page, primary=true, cascade=[persist, remove]}
- * @property Nextras\Orm\Relationships\OneHasOne|Ytnuk\Menu\Entity $menu {1:1 Ytnuk\Menu\Entity::$page, primary=true}
+ * @property Nextras\Orm\Relationships\OneHasOne|Ytnuk\Translation\Entity $content {1:1 Ytnuk\Translation\Entity, oneSided=true, isMain=true, cascade=[persist, remove]}
+ * @property Nextras\Orm\Relationships\OneHasOne|Ytnuk\Menu\Entity $menu {1:1 Ytnuk\Menu\Entity, oneSided=true, isMain=true}
  */
 final class Entity
 	extends Ytnuk\Orm\Entity
