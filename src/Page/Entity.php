dfa13ff0d92096d54b2179f9a40a0f5257669650
Vít Kutný <vitkutny@icloud.com>
2016-01-03 05:56:11 +0100

    cascade remove, form relations init, translation container

--- src/Page/Entity.php
+++ src/Page/Entity.php
@@ -7,7 +7,7 @@ use Ytnuk;
 /**
  * @property int $id {primary}
  * @property Nextras\Orm\Relationships\OneHasOne|Ytnuk\Translation\Entity $content {1:1 Ytnuk\Translation\Entity, oneSided=true, isMain=true, cascade=[persist, remove]}
- * @property Nextras\Orm\Relationships\OneHasOne|Ytnuk\Menu\Entity $menu {1:1 Ytnuk\Menu\Entity, oneSided=true, isMain=true}
+ * @property Nextras\Orm\Relationships\OneHasOne|Ytnuk\Menu\Entity $menu {1:1 Ytnuk\Menu\Entity, oneSided=true, isMain=true, cascade=[persist, remove]}
  */
 final class Entity
 	extends Ytnuk\Orm\Entity
